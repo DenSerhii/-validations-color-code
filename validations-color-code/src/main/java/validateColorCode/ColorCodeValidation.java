ce372458d0e8ace36b38e4563d0b994208c2619b
Serhii <106743687+DenSerhii@users.noreply.github.com>
2022-09-27 13:58:04 +0300

    add file

---
+++ validations-color-code/src/main/java/validateColorCode/ColorCodeValidation.java
@@ -0,0 +1,18 @@
+package validateColorCode;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ColorCodeValidation {
+    public static boolean validateColorCode(String color) {
+
+        // Put your code here
+
+        return false;
+    }
+}
+
+
+
+
+
